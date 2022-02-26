c5990386d89293944b5b177d5b257fedfcd725a3
LAPTOP-P861Q9K2\huawei <fkzxy4213@qq.com>
2022-02-26 22:58:07 +0800

    accumulation cycle and fstream added

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <cstdio>
 #include <windows.h>
 #include<string.h>
+#include<iostream>
+#include<fstream>
 using namespace std ;
 typedef long long ll;
-const int N = 10240; // matrix size
+const int N = 10230; // matrix size
+const double INTERVAL = 1;
 
 double b[N] [N] , col_sum[N] , a[N];
+ofstream outfile;
 
 void init () // generate a N*N matrix
 {
@@ -18,41 +22,53 @@ void init () // generate a N*N matrix
 }
 void col_plain(int n)
 {
-    ll head, tail , freq ;
+    ll head, tail , freq ,counter(0);
     QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
     // start time
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    for (int i = 0; i < n; i++) {
-        col_sum[ i ] = 0.0;
-        for (int j = 0; j < n; j++)
-            col_sum[ i ] += b[ j ] [ i ] * a[j];
+    tail = head;
+    while(INTERVAL*freq>(tail-head)*1000)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            col_sum[ i ] = 0.0;
+            for (int j = 0; j < n; j++)
+                col_sum[ i ] += b[ j ] [ i ] * a[j];
+        }
+        //end time
+        QueryPerformanceCounter((LARGE_INTEGER *)&tail);
+        counter++;
     }
-    //end time
-    QueryPerformanceCounter((LARGE_INTEGER *)&tail);
-    printf("%f\t", (tail-head)*1000.0/freq);
+    outfile<<(tail-head)*1000.0/freq/counter<<","<<counter<<",";
 }
 void col_cached(int n)
 {
-    ll head, tail , freq ;
+    ll head, tail , freq ,counter(0);
     QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
     // start time
     QueryPerformanceCounter((LARGE_INTEGER *)&head);
-    for (int i = 0; i < n; i++) {
-        col_sum[ i ] = 0.0;
-        for (int j = 0; j < n; j++)
-            for(i = 0;i < n;i++)
-                col_sum[ i ] += b[ j ] [ i ] * a[j] ;
+    while(INTERVAL*freq>(tail-head)*1000)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            col_sum[ i ] = 0.0;
+            for (int j = 0; j < n; j++)
+                for(i = 0;i < n;i++)
+                    col_sum[ i ] += b[ j ] [ i ] * a[j] ;
+        }
+        //end time
+        QueryPerformanceCounter((LARGE_INTEGER *)&tail);
+        counter++;
     }
-    //end time
-    QueryPerformanceCounter((LARGE_INTEGER *)&tail);
-    printf("%f\n", (tail-head)*1000.0/freq);
+    outfile<<(tail-head)*1000.0/freq/counter<<","<<counter<<endl;
 }
 int main()
 {
     init ();
+    outfile.open("output.txt", ios::out);
     for(int i=0;i<N;i+=10)
     {
-        printf("%d\t", i);
+        outfile<<i<<",";
         col_plain(i);
         col_cached(i);
     }
